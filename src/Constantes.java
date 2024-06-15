3b9c0a7486a4bbda8b2a6e302239cd85b85a322e
alex <alexinholozano10@gmail.com>
2024-06-14 21:30:48 -0500

    Constantes

---
+++ src/Constantes.java
@@ -0,0 +1,14 @@
+
+public class Constantes {
+
+	public static void main(String[] args) {
+		final int numero = 20;
+		final double decimal = 5.5;
+		int resulEnt = numero + 1;
+		double resulDeci = decimal - 1;
+		System.out.println("Entero desppues del incremento: " + resulEnt + "\n" +
+		"Desimal despues del decremento: " + resulDeci);
+
+	}
+
+}
